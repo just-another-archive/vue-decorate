49016eccb8281e128163eff64fcfd408ee89cccb
Julien Barbay <julien.barbay@linecorp.com>
2018-07-30 02:42:47 +0900

    better merge of events

--- src/component.js
+++ src/component.js
@@ -16,12 +16,31 @@ export default {
 
 
     ;['class', 'style',
-      'attrs', 'props', 'domProps',
-      'on', 'nativeOn',
-      'directives', 'ref']
+    'attrs', 'props', 'domProps',
+    'directives', 'ref']
       .filter(key => key in context.data)
       .forEach(key => {
         child.data[key] = Object.assign({}, child.data[key], context.data[key])
+      })
+
+
+    ;['on', 'nativeOn']
+      .filter(key => key in context.data)
+      .forEach(handler => {
+        Object
+          .keys(context.data[handler])
+          .forEach(event => {
+            if (!(event in child.data[handler]))
+              return child.data[handler][event] = context.data[handler][event];
+
+            child.data[handler][event] = [child, context]
+              .map(o => {
+                return Array.isArray(o.data[handler][event])
+                      ?  o.data[handler][event]
+                      : [o.data[handler][event]]
+              })
+              .reduce((arr, sub) => arr.concat(sub), [])
+          })
       })
 
     return child
